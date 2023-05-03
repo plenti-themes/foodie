ce83a8ce14c6b68a7b67e32cd1e985762801a2e7
jamestagal <benjaminjameswaller@gmail.com>
2023-05-03 21:46:39 +1000

    import onMount from svelte

--- layouts/global/html.svelte
+++ layouts/global/html.svelte
@@ -1,14 +1,25 @@
 <script>
-    import Footer from './footer.svelte';
+  import { onMount } from 'svelte';
+  import Footer from './footer.svelte';
   import Head from './head.svelte';
   import Nav from './nav.svelte';
+  import Login from './login.svelte';
 
-  export let content, layout, allContent, allLayouts, env;
+  export let content, layout, allContent, allLayouts, env, user;
+
+  let hash;
+  onMount(async () => {
+    hash = window.location.hash;
+  });
 </script>
 
 <html lang="en">
 <Head title={content.filename} {env} {...content.fields} />
 <body id="top">
+  {#if user && $user.isAuthenticated}
+    <svelte:component this={$user.menu} {user} bind:content={content} />
+  {/if}
+  <Login bind:hash {user} />
   <header class="header" data-header="">
     <div class="container">
       <Nav />
